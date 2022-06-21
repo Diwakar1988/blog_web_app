985394afa0525f92dc873f9d51016283fb384f7d
Diwakar Mishra <dimishra@microsoft.com>
2022-06-21 19:03:40 +0530

    concerns/module and basic authentication added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-    has_many :comments
-    validates :title , presence: true
+    include Visible
+    has_many :comments, dependent: :destroy #this will delete all comments associated with an article when an article gets deleted/destroyed
+    validates :title , presence: true, length: {minimum: 3}
     validates :body , presence: true, length: {minimum: 10}
 end
